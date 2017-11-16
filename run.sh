c4a0fcc9b2c6003baae2f5d22ada22c2e541916f
bjohnson <bkj.322@gmail.com>
2017-11-16 13:00:58 -0500

    ...

--- run.sh
+++ run.sh
@@ -11,3 +11,13 @@ tar -xzvf data.tar.gz
 # Run SGM
 
 time python sgm.py
+
+# --
+# Run SGM w/ cuda
+
+time python sgm.py --cuda
+
+# --
+# RUN SGM optimized for special case of `m=0`
+
+time python sgm-0.py
