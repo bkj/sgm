e8f69da8478b7bc509f35601ee67ad9376c72a7b
Ben Johnson <bkj.322@gmail.com>
2018-09-26 21:06:09 -0400

    kasios

--- backends.py
+++ backends.py
@@ -10,20 +10,37 @@ from hashlib import md5
 from scipy import sparse
 from time import time
 
-from sgm import BaseSGM, BaseFusedSGM
+from sgm import BaseSGM, BaseFusedSGM, BaseSparseSGM
 
-class ScipySparseSGM(BaseSGM):
-    def solve_lap(self, cost):
-        if isinstance(cost, sparse.csr_matrix):
-            cost = cost.toarray()
-        
-        cols = np.arange(cost.shape[0]).reshape(-1, cost.shape[0])
-        rows = cost.shape[0] * np.arange(cost.shape[0]).reshape(cost.shape[0], -1)
-        cost_ = cost + cols + rows
-        cost_ = cost_.max() - cost_
-        
-        _, idx, _ = lapjv(cost_)
-        return sparse.csr_matrix((np.ones(cost.shape[0]), (np.arange(cost.shape[0]), idx)))
+import sys
+sys.path.append('/home/bjohnson/projects/cuda_auction/python')
+from lap_auction import dense_lap_auction, csr_lap_auction, dot_auction
+
+# --
+# Helpers
+
+def _augment_cost(cost):
+    cols = np.arange(cost.shape[0]).reshape(-1, cost.shape[0])
+    rows = cost.shape[0] * np.arange(cost.shape[0]).reshape(cost.shape[0], -1)
+    return cost + cols + rows
+
+def _lapjv(cost, augment):
+    if isinstance(cost, sparse.csr_matrix):
+        cost = cost.toarray()
+    
+    cost_ = _augment_cost(cost) if augment else cost
+    cost_ = cost_.max() - cost_
+    
+    _, idx, _ = lapjv(cost_)
+    return sparse.csr_matrix((np.ones(cost.shape[0]), (np.arange(cost.shape[0]), idx)))
+
+# --
+# Backends
+
+class ScipySGM(BaseSGM):
+    """ Basic scipy SGM -- keeps some parts sparse"""
+    def solve_lap(self, cost, augment=True):
+        return _lapjv(cost, augment)
         
     def compute_grad(self, A, P, B):
         AP = A.dot(P)
@@ -36,26 +53,16 @@ class ScipySparseSGM(BaseSGM):
 
 
 class ScipyFusedSGM(BaseFusedSGM):
-    def solve_lap(self, cost):
-        if isinstance(cost, sparse.csr_matrix):
-            cost = cost.toarray()
-        
-        _, idx, _ = lapjv(cost.max() - cost)
-        
-        return sparse.csr_matrix((np.ones(cost.shape[0]), (np.arange(cost.shape[0]), idx)))
+    """ 
+        Fuses multiplication and LAP
+        More sparse operations
+    """
+    def solve_lap(self, cost, augment=True):
+        return _lapjv(cost, augment)
     
-    def solve_lap_fused(self, A, P, B):
+    def solve_lap_fused(self, A, P, B, augment=True):
         cost = A.dot(P).dot(B)
-        
-        if isinstance(cost, sparse.csr_matrix):
-            cost = cost.toarray()
-        
-        t = time()
-        _, idx, _ = lapjv(cost.max() - cost)
-        print('lap_time', int(1000 * (time() - t)))
-        
-        vals = cost[(np.arange(cost.shape[0]), idx)]
-        return sparse.csr_matrix((np.ones(cost.shape[0]), (np.arange(idx.shape[0]), idx)))
+        return _lapjv(cost, augment)
         
     def sparse_trace(self, A, X, B, Y):
         AX  = A.dot(X)
@@ -68,76 +75,101 @@ class ScipyFusedSGM(BaseFusedSGM):
 
 # >>
 
-import sys
-sys.path.append('/home/bjohnson/projects/cuda_auction/python')
-from lap_auction import dense_lap_auction, csr_lap_auction, dot_auction
-rng = np.random.RandomState(seed=111222333)
-_ = dense_lap_auction(rng.choice(100, (100, 100)), 
-    verbose=False,
-    num_runs=1,
-    auction_max_eps=10.0,
-    auction_min_eps=1.0,
-    auction_factor=0.0
-)
+# rng = np.random.RandomState(seed=111222333)
+# _ = dense_lap_auction(rng.choice(100, (10, 10)), 
+#     verbose=False,
+#     num_runs=1,
+#     auction_max_eps=1..0,
+#     auction_min_eps=1.0,
+#     auction_factor=0.0
+# )
+
+class AuctionSGM(ScipySGM):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.iter = 0
+    
+    def solve_lap(self, cost, augment=True):
+        if self.iter < 4:
+            print('csr_lap_auction')
+            idx = csr_lap_auction(cost,
+                verbose=True,
+                num_runs=1,
+                auction_max_eps=1.0,
+                auction_min_eps=1.0,
+                auction_factor=0.0
+            )
+            self.iter += 1
+            return sparse.csr_matrix((np.ones(cost.shape[0]), (np.arange(idx.shape[0]), idx)))
+        else:
+            print('lapjv')
+            return _lapjv(cost, augment)
+
+
+
+class AuctionSparseSGM(BaseSparseSGM):
+    """
+        Fuses multiplication and LAP
+        More sparse operations
+        Use auction algorithm
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.iter = 0
+        
+    def sparse_trace(self, A, X, B, Y):
+        AX  = A.dot(X)
+        YBt = Y.dot(B.T)
+        
+        AX_sum = Y.dot(AX.sum(axis=1)).sum()
+        B_sum  = Y.T.dot(B.sum(axis=0).T).sum()
+        
+        return 4 * AX.multiply(YBt).sum() + A.shape[0] * Y.sum() - 2 * (AX_sum + B_sum)
+    
+    def solve_lap(self, cost, augment=True):
+        t = time()
+        idx = csr_lap_auction(cost,
+            verbose=True,
+            num_runs=1,
+            auction_max_eps=1.0,
+            auction_min_eps=1.0,
+            auction_factor=0.0
+        )
+        # print('solve_lap_fused: csr_lap_auction', time() - t)
+        self.iter += 1
+        return sparse.csr_matrix((np.ones(cost.shape[0]), (np.arange(idx.shape[0]), idx)))
+
+
+# --
+# Fused
+
+class ScipyFusedSGM(BaseFusedSGM):
+    """ 
+        Fuses multiplication and LAP
+        More sparse operations
+    """
+    def solve_lap(self, cost, augment=True):
+        return _lapjv(cost, augment)
+    
+    def solve_lap_fused(self, AP, B, augment=True):
+        cost = AP.dot(B)
+        return _lapjv(cost, augment)
+        
+    def sparse_trace(self, AX, B, Y):
+        YBt = Y.dot(B.T)
+        
+        AX_sum = Y.dot(AX.sum(axis=1)).sum()
+        B_sum  = Y.T.dot(B.sum(axis=0).T).sum()
+        
+        return 4 * AX.multiply(YBt).sum() + AX.shape[0] * Y.sum() - 2 * (AX_sum + B_sum)
+
 
 class AuctionFusedSGM(ScipyFusedSGM):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.iter = 0
     
-    def solve_lap_fused(self, A, P, B):
-        # idx = dense_lap_auction(A.dot(P).dot(B).toarray(),
-        #     verbose=True,
-        #     num_runs=1,
-        #     auction_max_eps=1.0,
-        #     auction_min_eps=1.0,
-        #     auction_factor=0.0
-        # )
-        
-        if self.iter < 4:
-            print('csr_lap_auction')
-            idx = csr_lap_auction(A.dot(P).dot(B), 
-                verbose=True,
-                num_runs=1,
-                auction_max_eps=1.0,
-                auction_min_eps=1.0,
-                auction_factor=0.0
-            )
-        else:
-            print('lapjv')
-            cost = A.dot(P).dot(B).toarray()
-            _, idx, _ = lapjv(cost.max() - cost)
-        
-        # idx = dot_auction(A.dot(P), B, 256)
-        
+    def solve_lap_fused(self, AP, B, augment=True):
+        idx = dot_auction(AP, B, AP.shape[0])
         self.iter += 1
-        return sparse.csr_matrix((np.ones(A.shape[0]), (np.arange(idx.shape[0]), idx)))
-
-
-class AuctionSparseSGM(ScipySparseSGM):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.iter = 0
-    
-    def solve_lap(self, cost):
-        if self.iter < 4:
-            print('csr_lap_auction')
-            idx = csr_lap_auction(cost, 
-                verbose=True,
-                num_runs=1,
-                auction_max_eps=1.0,
-                auction_min_eps=1.0,
-                auction_factor=0.0
-            )
-        else:
-            print('lapjv')
-            cost = cost.toarray()
-            _, idx, _ = lapjv(cost.max() - cost)
-        
-        # idx = dot_auction(A.dot(P), B, 256)
-        
-        self.iter += 1
-        return sparse.csr_matrix((np.ones(cost.shape[0]), (np.arange(idx.shape[0]), idx)))
-
-
-# <<
+        return sparse.csr_matrix((np.ones(AP.shape[0]), (np.arange(idx.shape[0]), idx)))
--- sgm.py
+++ sgm.py
@@ -17,132 +17,161 @@ import sys
 import json
 from time import time
 
-def _check_convergence(c, d, e):
-    cde = c + e - d 
-    d2e = d - 2 * e
-    
-    if (cde == 0) and (d2e == 0):
-        alpha = 0
-        falpha = -1 # NA value
-    else:
-        if (cde == 0):
-            alpha  = float('inf')
-            falpha = -1 # NA value
-        else:
-            alpha = -d2e / (2 * cde)
-            falpha = cde * alpha ** 2 + d2e * alpha
-    
-    f1 = c - e
-    
-    return alpha, f1, falpha
 
-import numpy as np
-class BaseSGM:
+
+
+class _BaseSGM:
     def __reset_timers(self):
         self.lap_times   = []
         self.grad_times  = []
         self.check_times = []
+        self.start_time  = time()
     
+    def check_convergence(self, c, d, e, tolerance):
+        cde = c + e - d 
+        d2e = d - 2 * e
+        
+        if (cde == 0) and (d2e == 0):
+            alpha = 0
+            falpha = -1 # NA value
+        else:
+            if (cde == 0):
+                alpha  = float('inf')
+                falpha = -1 # NA value
+            else:
+                alpha = -d2e / (2 * cde)
+                falpha = cde * alpha ** 2 + d2e * alpha
+        
+        f1 = c - e
+        
+        if (alpha > 0) and (alpha < tolerance) and (falpha > max(0, f1)):
+            return alpha, False # P <- (alpha * P) + (1 - alpha) * T
+        elif f1 < 0:
+            return None, False # P <- T
+        else:
+            return None, True  # stop
+
+
+class BaseSGM(_BaseSGM):
     def run(self, A, P, B, num_iters, tolerance, verbose=True):
         self.__reset_timers()
-        start_time = time()
-        
-        t = time()
         sparse_grad, dense_grad = self.compute_grad(A, P, B)
-        self.grad_times.append(time() - t)
         
         stop = False
         for i in range(num_iters):
             if verbose:
-                print('iter=%d | %fs' % (i, time() - start_time), file=sys.stderr)
+                print('iter=%d | %fs' % (i, time() - self.start_time), file=sys.stderr)
             
-            t = time()
             T = self.solve_lap(sparse_grad)
-            self.lap_times.append(time() - t)
             
-            t = time()
             sparse_gradt, dense_gradt = self.compute_grad(A, T, B)
-            self.grad_times.append(time() - t)
             
-            t = time()
             ps_grad_P  = self.prod_sum(sparse_grad + dense_grad, P)
             ps_grad_T  = self.prod_sum(sparse_grad + dense_grad, T)
             ps_gradt_P = self.prod_sum(sparse_gradt + dense_gradt, P)
             ps_gradt_T = self.prod_sum(sparse_gradt + dense_gradt, T)
             
-            alpha, f1, falpha = _check_convergence(
+            alpha, stop = self.check_convergence(
                 c=ps_grad_P,
                 d=ps_gradt_P + ps_grad_T,
-                e=ps_gradt_T
+                e=ps_gradt_T,
+                tolerance=tolerance,
             )
-            
-            if (alpha > 0) and (alpha < tolerance) and (falpha > max(0, f1)):
-                P           = (alpha * P)    + (1 - alpha) * T
-                sparse_grad = (alpha * sparse_grad) + (1 - alpha) * sparse_gradt
-                dense_grad  = (alpha * dense_grad) + (1 - alpha) * dense_gradt
-            elif f1 < 0:
-                P           = T
-                sparse_grad = sparse_gradt
-                dense_grad  = dense_gradt
-            else:
-                stop = True
-            
-            self.check_times.append(time() - t)
             
             if stop:
                 break
+            
+            if alpha is not None:
+                P           = (alpha * P)           + (1 - alpha) * T
+                sparse_grad = (alpha * sparse_grad) + (1 - alpha) * sparse_gradt
+                dense_grad  = (alpha * dense_grad)  + (1 - alpha) * dense_gradt
+            else:
+                P           = T
+                sparse_grad = sparse_gradt
+                dense_grad  = dense_gradt
         
-        t = time()
         P_out = self.solve_lap(P)
-        self.lap_times.append(time() - t)
         return P_out
 
+# =====================================
 
-class BaseFusedSGM:
-    def __reset_timers(self):
-        self.lap_times   = []
-        self.grad_times  = []
-        self.check_times = []
-    
+class BaseSparseSGM(_BaseSGM):
     def run(self, A, P, B, num_iters, tolerance, verbose=True):
         self.__reset_timers()
-        start_time = time()
+        
+        grad = A.dot(P).dot(B)
         
         stop = False
         for i in range(num_iters):
             if verbose:
-                print('iter=%d | %fs' % (i, time() - start_time), file=sys.stderr)
+                print('iter=%d | %fs' % (i, time() - self.start_time), file=sys.stderr)
             
-            t = time()
-            T = self.solve_lap_fused(A, P, B)
-            self.lap_times.append(time() - t)
+            T = self.solve_lap(grad)
             
-            t = time()
+            gradt = A.dot(T).dot(B)
+            
             # Could avoid recomputation here
             ps_grad_P  = self.sparse_trace(A, P, B, P)
             ps_grad_T  = self.sparse_trace(A, P, B, T)
             ps_gradt_P = self.sparse_trace(A, T, B, P)
             ps_gradt_T = self.sparse_trace(A, T, B, T)
             
-            alpha, f1, falpha = _check_convergence(
+            alpha, stop = self.check_convergence(
                 c=ps_grad_P,
                 d=ps_gradt_P + ps_grad_T,
-                e=ps_gradt_T
+                e=ps_gradt_T,
+                tolerance=tolerance,
             )
-            
-            if (alpha > 0) and (alpha < tolerance) and (falpha > max(0, f1)):
-                P = (alpha * P) + (1 - alpha) * T
-            elif f1 < 0:
-                P = T
-            else:
-                stop = True
-            
-            self.check_times.append(time() - t)
             
             if stop:
                 break
+            
+            if alpha is not None:
+                P    = (alpha * P) + (1 - alpha) * T
+                grad = (alpha * grad) + (1 - alpha) * gradt
+            else:
+                P = T
+                grad = gradt
         
-        t = time()
         P_out = self.solve_lap(P)
-        self.lap_times.append(time() - t)
+        return P_out
+
+# =====================================
+
+class BaseFusedSGM(_BaseSGM):
+    def run(self, A, P, B, num_iters, tolerance, verbose=True):
+        self.__reset_timers()
+        
+        stop = False
+        AP = A.dot(P)
+        for i in range(num_iters):
+            if verbose:
+                print('iter=%d | %fs' % (i, time() - self.start_time), file=sys.stderr)
+            
+            T = self.solve_lap_fused(AP, B)
+            
+            AT = A.dot(T)
+            ps_grad_P  = self.sparse_trace(AP, B, P)
+            ps_grad_T  = self.sparse_trace(AP, B, T)
+            ps_gradt_P = self.sparse_trace(AT, B, P)
+            ps_gradt_T = self.sparse_trace(AT, B, T)
+            
+            alpha, stop = self.check_convergence(
+                c=ps_grad_P,
+                d=ps_gradt_P + ps_grad_T,
+                e=ps_gradt_T,
+                tolerance=tolerance,
+            )
+            
+            if stop:
+                break
+            
+            if alpha is not None:
+                P  = (alpha * P)  + (1 - alpha) * T
+                AP = (alpha * AP) + (1 - alpha) * AT
+            else:
+                P  = T
+                AP = AT
+        
+        P_out = self.solve_lap(P)
         return P_out
