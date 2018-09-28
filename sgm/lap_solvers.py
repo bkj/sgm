26d0742c9dbec0c2eb17d59797b2770a692cf9a0
bjohnson <bkj.322@gmail.com>
2018-09-28 00:05:20 -0400

    error handling

--- sgm/lap_solvers.py
+++ sgm/lap_solvers.py
@@ -11,17 +11,21 @@ import torch
 
 from lap import lapjv
 
-sys.path.append('/home/bjohnson/projects/cuda_auction/python')
-from lap_auction import dense_lap_auction, csr_lap_auction, dot_auction
+try:
+    sys.path.append('/home/bjohnson/projects/cuda_auction/python')
+    from lap_auction import dense_lap_auction, csr_lap_auction, dot_auction
+except:
+    print('WARNING: sgm.lap_solvers cannot load `lap_auction`', file=sys.stderr)
 
 def _gatagat_lapjv(cost):
-    if isinstance(cost, torch.Tensor):
+    if isinstance(cost, torch.Tensor) or isinstance(cost, torch.cuda.FloatTensor):
         cost_ = cost.cpu().numpy()
     elif isinstance(cost, sparse.csr_matrix):
         cost_ = cost.toarray()
     elif isinstance(cost, np.ndarray):
         cost_ = cost
     else:
+        print(type(cost))
         raise Exception('_gatagat_lapjv: cost has unknown type!')
     
     _, idx, _ = lapjv(cost_.max() - cost_)
