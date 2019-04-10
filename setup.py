e785cd6874482e87d4d95ed14864629074dd917f
fred <fred.battista@qntfy.com>
2019-04-10 14:32:14 -0700

    Installing from pip entirely

--- setup.py
+++ setup.py
@@ -7,5 +7,15 @@ setup(
     author="...",
     classifiers=[],
     packages=['sgm'],
+    install_requires=[
+        'cython==0.29.3',
+        'tqdm',
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'git+git://github.com/gatagat/lap.git',
+        'git+git://github.com/src-d/lapjv',
+    ],
     version="1.0"
 )
